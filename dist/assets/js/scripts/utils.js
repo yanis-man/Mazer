4ec1fd124d726066dc43b5ad79e4ddea1fe7274d
Yanis <59359557+yanis-man@users.noreply.github.com>
2021-07-27 02:02:59 +0200

    add function to return week num

--- dist/assets/js/scripts/utils.js
+++ dist/assets/js/scripts/utils.js
@@ -13,4 +13,11 @@ function get_url(url, params)
         xhr.send(params);
     return result
 }
-export {get_url};
+
+function getNumberOfWeek() {
+    const today = new Date();
+    const firstDayOfYear = new Date(today.getFullYear(), 0, 1);
+    const pastDaysOfYear = (today.valueOf() - firstDayOfYear.valueOf()) / 86400000;
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7)-1;
+}
+export {get_url, getNumberOfWeek};
