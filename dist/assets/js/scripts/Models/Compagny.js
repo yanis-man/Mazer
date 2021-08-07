bb925849815cc338cc9547f1caefffba1ca3c18d
Yanis <59359557+yanis-man@users.noreply.github.com>
2021-08-07 13:05:59 +0200

    Add compagny objet model

--- dist/assets/js/scripts/Models/Compagny.js
+++ dist/assets/js/scripts/Models/Compagny.js
@@ -1,7 +1,23 @@
+import {ApiURL} from '../../CONFIG'
+import {get_url} from '../utils.js'
+
 class Compagny
 {
-    constructor(owner_id)
+    constructor()
     {
+        const compagnyData = get_url(ApiURL.COMP_URL, "action=retrieveData")['data'];
         
+        this.to = compagnyData[0][0]['total_to'];
+
+        this.head_rate = compagnyData[1][0]['salary'];
+        this.conf_emp_rate = compagnyData[1][0]['salary'];
+        this.test_emp_rate = compagnyData[1][0]['salary'];
+        this.intern_rate = compagnyData[1][0]['salary'];
+    }
+    update_display(document)
+    {
+        document.getElementById("comp_to").innerHTML = this.to;
     }
 }
+
+export default Compagny
